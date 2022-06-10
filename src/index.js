7a27524bdcee9b63acb7eab8fcc7383982f72052
constanzadl <constanzadl94@gmail.com>
2022-06-09 21:20:15 -0600

    converted class components into functional components

--- src/index.js
+++ src/index.js
@@ -2,49 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+const Square = () => {
+  return (
+    <button className="square">
+    {/* TODO */}
+  </button>
+  )
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
+const renderSquare = (i) => {
+  return (
+    <Square />
+  )
+}
 
-  render() {
-    const status = 'Next player: X';
+const Board = () => {
+  const status = 'Next player: X';
 
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
-class Game extends React.Component {
-  render() {
+const Game = () => {
     return (
       <div className="game">
         <div className="game-board">
@@ -55,9 +52,9 @@ class Game extends React.Component {
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    );
-  }
+      )
 }
+
 
 // ========================================
 
